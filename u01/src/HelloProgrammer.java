205409e32d19e5ad60c4ff345f2b232e93251e12
Ray <rayan.behr@icloud.com>
2021-09-29 06:54:37 +0200

    completed tasks 1, 2 and 3

--- u01/src/HelloProgrammer.java
+++ u01/src/HelloProgrammer.java
@@ -1,7 +1,17 @@
 
+/* A1
+ * Event: EProg
+ * Author: Ray
+ */
+
+
 public class HelloProgrammer {
     
+	//A2
     public static void main(String[] args) {
-        System.out.println("Hello Programmer");
+        System.out.println("Hello, my name is Ray");
     }
+    
+    //A3
+    //Committed changes to master
 }
