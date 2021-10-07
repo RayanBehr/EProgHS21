8c142962e9ff913e293ab862803cca843cd8ff78
Ray <rayan.behr@icloud.com>
2021-10-07 21:03:54 +0200

    first changes to u2

--- u02/src/AbsoluteMax.java
+++ u02/src/AbsoluteMax.java
@@ -1,16 +1,27 @@
 
 public class AbsoluteMax {
 	
-	public static void main(String[] args) {
-		// Aendern Sie die Werte um verschiedene Ausfuehrungen zu testen.
-		int a = 4;
-		int b = -5;
-		int c = 3;
+	public static void main(String[] args) { new AbsoluteMax(4, -5, 3, -1); }
+	
+	// Aendern Sie die Werte um verschiedene Ausfuehrungen zu testen.
+	int a, b, c;
+	
+	// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
+	int r;
+
+
+	public AbsoluteMax(int a, int b, int c, int r) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.r = r;
 		
-		// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
-		int r = -1;
-		
+		sysout();
+	}
+	
+	void sysout() {
 		// Der finale Wert von r wird ausgegeben
-		System.out.println("a: " + a + ", b: " + b + ", c: " + c + " --> r: " + r);
+		System.out.println("a: " + a + ", b: " + b + 
+					       ", c: " + c + " --> r: " + r);
 	}
 }
--- u02/src/FollerVehler.java
+++
@@ -1,9 +0,0 @@
-public class FollerVehler {
-    public static main(String args) {
-        System.out.println(Hello world);
-        system.out.Pritnln("Gefällt Ihnen dieses Programm"?);
-        System.out.println()
-
-        System.println("Ich habe es selbst geschrieben.";
-    {
-}
