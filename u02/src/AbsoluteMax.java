d111d1c6f8e579b0492ca8521496f7d3286b2613
Ray <rayan.behr@icloud.com>
2021-10-10 22:31:17 +0200

    bigbrain

--- u02/src/AbsoluteMax.java
+++ u02/src/AbsoluteMax.java
@@ -1,37 +1,74 @@
+
 public class AbsoluteMax {
 	
-	public static void main(String[] args) { new AbsoluteMax(4, -5, 3, -1); }
+	public static void main(String[] args) { 	
+		long n = Long.parseLong("139472305678615");
+		long m = Long.parseLong("273426584985");
+		new AbsoluteMax( n, m); 
+	}
 	
 	// Aendern Sie die Werte um verschiedene Ausfuehrungen zu testen.
-	int a, b, c;
+	long m, n;
+	
+	long steps = 0;
 	
 	// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
-	int r;
+	long r;
 
 
-	public AbsoluteMax(int a, int b, int c, int r) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-		this.r = r; //
+	public AbsoluteMax(long n, long m) {
+		this.n = n;
+		this.m = m;
+
 		doStuff();
 	}
 	
 	void doStuff() {
-		r = gcd(28,21);		
+		System.out.println("using Naive Algo");
+		//r = gcd(n,m);
+		System.out.println("Program would not terminate in time");
+		sysout();
+
+		System.out.println("<------------>");
+		
+		System.out.println("using Euclid's Algo");
+		r = gcdFAST(n,m);
 		sysout();
 	}
 	
 	void sysout() {
 		// Der finale Wert von r wird ausgegeben
-		System.out.println("a: " + a + ", b: " + b + 
-					       ", c: " + c + " --> r: " + r);
+		System.out.println("n: " + n + ", m: " + m + " --> r: " + r + " (computed in: " + steps + " Steps)");
 	}
 	
-	int gcd(int n, int m) {
-		for(int d = m; d > 1; d--)
+	long gcd(long n, long m) {
+		steps = 0; 
+		for(long d = m; d > 1; d--) {
+			steps++; System.out.println(steps);
 			if(n%d == 0 && m%d == 0)
 				return d;
+			}
 		return 1;
 	}
+	
+	long gcdFAST(long n, long m) {
+		steps = 0;
+		long d; long dd; long ddd;
+		d = n; dd = m; ddd = d % dd;	
+		System.out.println("( " + d + ", " + dd + ", " + ddd + " )");
+		while(true) {
+			if( ddd != 0 ) { d = dd % ddd; steps++; }
+			else { return dd;  }
+			System.out.println("( " + d + ", " + dd + ", " + ddd + " )");
+			
+			if( d != 0   ) { dd = ddd % d; steps++; }
+			else { return ddd; }
+			System.out.println("( " + d + ", " + dd + ", " + ddd + " )");
+			
+			if( dd != 0  ) { ddd = d % dd; steps++; }
+			else { return d;   }			
+		}	
+		
+	}
+			
 }
