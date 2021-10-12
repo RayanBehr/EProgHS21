09926a70e7b3dd9d291fff0f0d6895d6e78bf4ac
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2021-10-12 20:06:46 +0200

    Publish u03

---
+++ u03/src/Calculations.java
@@ -0,0 +1,18 @@
+
+public class Calculations {
+	
+	public static int checksum(int x) {
+		// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
+		return -1;
+	}
+	
+	public static boolean magic7(int a, int b) {
+		// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
+		return false;
+	}
+	
+	public static boolean fast12(int z) {
+		// TODO: Vervollstaendigen Sie den Code wie in der Aufgabenbeschreibung beschrieben
+		return false;
+	}
+}
