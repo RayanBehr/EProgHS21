910719ed45f8dcbc1ba31670b20b3989faf960bf
Ray <rayan.behr@icloud.com>
2021-10-19 20:41:47 +0200

    idea

--- u03/src/Zahlen.java
+++ u03/src/Zahlen.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+
+import javax.swing.JFrame;
 /*
- * Author: 
+ * Author: Rayan Behr
  * 
  * 
  * Dieses Programm liest einen String ein, der eine Siebensegmentanzeige kodiert, und gibt die kodierte Zahl als Integer aus.
  */
-public class Zahlen {
+public class Zahlen extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO 
