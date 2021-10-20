2b77a8269c8bc4ddaebcc9d3ebf42e559f255225
Ray <rayan.behr@icloud.com>
2021-10-20 08:47:06 +0200

    done

--- u04/src/Newton.java
+++ u04/src/Newton.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
 
 public class Newton {
+	
+	
+	///*
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Input: ");
+		System.out.println("Sqrt of input: " + 
+							rapsonSqrt(s.nextInt()));
+		
+		s.close();
+	}//*/
 
+	public static double rapsonSqrt(double c) {
+		double t = c / 2;
+	
+		while(Math.abs(t*t) < 0.0000000000001) {
+			t = (c / t + t) / 2;
+		}
+		
+		return t;
+	}
+	
 }
