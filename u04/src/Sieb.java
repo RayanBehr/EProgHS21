e22eb1876464fe88e8c4b01250c33099bb6fa252
Ray <rayan.behr@icloud.com>
2021-10-26 22:26:46 +0200

    tweek

--- u04/src/Sieb.java
+++ u04/src/Sieb.java
@@ -9,12 +9,12 @@ public class Sieb {
 		
 		System.out.print("Upper limit: ");
 		System.out.println("Number of primes up to inclusive limit: " + 
-							anzahlPrimzahlenbis(s.nextInt()));
+							anzahlPrimzahlenBis(s.nextInt()));
 		
 		s.close();
 	}//*/
 	
-	public static int anzahlPrimzahlenbis(int limit) {
+	public static int anzahlPrimzahlenBis(int limit) {
 		boolean[] notPr = new boolean[limit + 1];
 		notPr[0] = true; notPr[1] = true;
 		
@@ -26,9 +26,24 @@ public class Sieb {
 		
 		int cnt = 0;
 		for(int i = 0; i <= limit; i++) {
-			if(notPr[i] == false) cnt++;
+			if(notPr[i] == false) { cnt++;
+			System.out.print(i + " ");
+			if(cnt%10 == 0)System.out.println();
+			}
+			
 		}
-		
+		System.out.println();
 		return cnt;
 	}
+	
+	/*
+	public static int anzahlPrimzahlenBisElegant(int limit) {
+
+		int cnt = limit;
+		for(int i = 2; i <= limit; i++)
+			for(int v = i+i; v <= limit; v += i)
+				cnt--;
+
+		return cnt;
+	}*/ //falsch
 }
