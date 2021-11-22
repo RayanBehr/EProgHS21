9494881e3822ec5f216314442592615dacb871e3
Ray <rayan.behr@icloud.com>
2021-11-22 22:49:13 +0100

    playing some golf

--- u08/src/Labyrinth.java
+++ u08/src/Labyrinth.java
@@ -7,8 +7,7 @@ public class Labyrinth {
 		return rec1(room, sum);
 	}
 	
-	public static boolean rec1(Room room, int sum) {
-			
+	public static boolean rec1(Room room, int sum) {		
 		// abort if sum of ages leading to this is divisible by three
 		if(sum %3 == 0) return false;
 		
@@ -21,35 +20,51 @@ public class Labyrinth {
 			
 			if(ajRoom != room && //go to the same room
 					rec1(ajRoom, nextSum))
-				return true;			}
-		
+				return true;			}		
 		return false;
 	}
 	
-	public static boolean task2(Room room) {		
-		
-		Stack<Room> memoA = new Stack<>(); 
-		Stack<Room> memoB = new Stack<>(); 	
-		
-		for(Room ajRoomA : room.doorsTo)
-			for(Room ajRoomB : room.doorsTo) 
-			{ // we try to find two ajecent rooms with the same age									
-				if(ajRoomA.getAge() == ajRoomB.getAge())
-				{
-					// we add the ajecent rooms to the memo
-					memoA.push(ajRoomA); memoB.push(ajRoomB);
-					
-					//if the recursion evaluates to true we found it
-					if(rec2(ajRoomA, ajRoomB, copy(memoA), copy(memoB)))
-						return true;	
-					
-					// we remove the rooms we just pushed, because we want to try another combination
-					memoA.pop(); memoB.pop(); //revert memo
-				}
-			}		
+	public static boolean task2(Room room) {			
+		Stack<Room> memoA = new Stack<>(); Stack<Room> memoB = new Stack<>(); 		
+		for(Room ajRoomA : room.doorsTo) for(Room ajRoomB : room.doorsTo)							
+			if(ajRoomA.getAge() == ajRoomB.getAge()) {
+				memoA.push(ajRoomA); memoB.push(ajRoomB);		
+				if(rec2(ajRoomA, ajRoomB, copy(memoA), copy(memoB))) return true;	
+				memoA.pop(); memoB.pop(); }	
 		return false;
 	}
 	
+	public static boolean rec2(Room roomA, Room roomB, Stack<Room> memoA, Stack<Room> memoB) {
+		for(Room ajRoomA : roomA.doorsTo) for(Room ajRoomB : roomB.doorsTo)			
+			if(ajRoomA.getAge() == ajRoomB.getAge()) {			
+				memoA.push(ajRoomA); memoB.push(ajRoomB);
+				if(rec2(ajRoomA, ajRoomB, copy(memoA), copy(memoB))) return true;			
+				memoA.pop(); memoB.pop(); }		
+		if(roomA.isExit() && roomB.isExit())
+			while(memoA.size() > 0) if(memoA.pop() != memoB.pop()) return true;				
+		return false;
+	}
+	
+	public static Stack<Room> copy(Stack<Room> st) {	
+		Stack<Room> flip = new Stack<>();
+		while(st.size() > 0) flip.push(st.pop());		
+		Stack<Room> stCopy = new Stack<>();
+		while(flip.size() > 0) {
+			Room ele = flip.pop();
+			stCopy.push(ele); st.push(ele); }	
+		return stCopy;	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 	public static boolean rec2(Room roomA, Room roomB, Stack<Room> memoA, Stack<Room> memoB) {
 		
 		// if we have exits it won't run 'for each'
@@ -73,32 +88,28 @@ public class Labyrinth {
 		if(roomA.isExit() && roomB.isExit())
 		{
 			// we backup our memo
-			Stack<Room> memoSaveA = copy(memoA); Stack<Room> memoSaveB = copy(memoB); 
+			//Stack<Room> memoSaveA = copy(memoA); Stack<Room> memoSaveB = copy(memoB); 
 			
 			// we only want to return true if they are different somewhere
 			while(memoA.size() > 0) // stacks have same size
 				if(memoA.pop() != memoB.pop()) {
-					/*
+					
 					for(int i = 0; i < memoSaveA.size(); i++) {
 						Room a = memoSaveA.pop();
 						Room b = memoSaveB.pop();						
 						System.out.println("Reference: " + a + ",	Age: " + a.getAge() + "	" + 
 									       "Reference: " + b + ",	Age: " + b.getAge());
 					}
-					System.out.println("--------------------");*/
+					System.out.println("--------------------");
 					return true;
 				}		
 			// we restore our memo
-			memoA = memoSaveA; memoB = memoSaveB;	
+			//memoA = memoSaveA; memoB = memoSaveB;	
 		}
 		
 		return false;
 	}
 	
-	
-	/*
-	 * return Returns a copied Stack with the same elements
-	 */
 	public static Stack<Room> copy(Stack<Room> st) {
 		
 		Stack<Room> flip = new Stack<>();
@@ -113,6 +124,7 @@ public class Labyrinth {
 			
 		return stCopy;	
 	}
+	*/
 	
 	
 }
