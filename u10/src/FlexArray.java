47a456a7184f698fb63e2df88449364de6facb0b
Ray <rayan.behr@icloud.com>
2021-12-07 17:39:42 +0100

    Bonnus Week 10

--- u10/src/FlexArray.java
+++ u10/src/FlexArray.java
@@ -1,56 +1,88 @@
-
 public class FlexArray {
-
-	FlexArray() {
-        // TODO
+    private int[] array;
+    private int base;
+    private int length;
+    
+    FlexArray() {
+        array = new int[0];
+        length = 0;
+        base = 0;
     }
 
     FlexArray(int size) {
-    	// TODO
+        array = new int[size];
+        length = size;
+        base = 0;
     }
 
     FlexArray(int size, int start) {
-    	// TODO
+        array = new int[size];
+        length = size;
+        base = start;
     }
 
     public int length() {
-    	// TODO
-        return 0;
+        return length;
     }
 
     public int base() {
-    	// TODO
-        return 0;
+        return base;
     }
 
     public int[] rawArray() {
-    	// TODO
-        return null;
+        return array.clone();
     }
 
     public int read(int index) {
-    	// TODO
-    	return 0;
+        return (isValid(index))? 
+        		array[index-base] : 
+        		Integer.MIN_VALUE;    		
     }
 
     public void write(int index, int value) {
-    	// TODO
+        if(isValid(index)) array[index-base] = value;
     }
 
-    public FlexArray copy() {
-    	// TODO
-    	return null;
+    public FlexArray copy() {    
+        FlexArray temp = new FlexArray();
+        temp.array = this.array.clone();
+        temp.length = this.length();
+        temp.base = this.base();
+        return temp;    
     }
 
     public FlexArray merge(FlexArray f) {
-    	// TODO
-    	return null;
+    	int start = Math.min(this.base, f.base());
+    	int max = Math.max(this.base + this.length(), f.base() + f.length());
+    	// if(this.base + this.length() < f.base() || f.base + f.length() < this.base()) return null;
+    	FlexArray mer = new FlexArray(max-start, start);
+    	for(int i = start; i < max; i++) {
+    		int sum = 0;	
+    		// if(!(this.isValid(i) || f.isValid(i))) return null; // De-Morgan's
+    		if(!this.isValid(i) && !f.isValid(i)) return null; 
+    		if(this.isValid(i)) sum += this.read(i);
+    		if(f.isValid(i)) sum += f.read(i);
+    		mer.write(i, sum); }
+    	return mer;
+    }
+    
+    public boolean isValid(int index) {
+        return index >= base && index < base+length;
     }
 
     @Override
-    public boolean equals(Object o) {
-    	// TODO
-    	return super.equals(o);
-
+    public boolean equals(Object o) {      
+        if(o.getClass() != this.getClass())
+            return false;
+        FlexArray temp = (FlexArray)o;
+        if(temp.length() != this.length())
+            return false;
+        if(temp.base() != this.base())
+            return false;
+        for(int i = 0; i < this.length ;i++)
+        	if(temp.rawArray()[i] != this.array[i])
+        		return false; 
+        return true;
     }
+    
 }
--- u10/test/FlexArrayTest.java
+++ u10/test/FlexArrayTest.java
@@ -152,4 +152,246 @@ public class FlexArrayTest {
         int[] expected3 = {0, 1, 102, 203, 304, 405, 6, 7, 8, 9};
         assertArrayEquals(expected3, ra);
 	}
+	
+	@Test
+	public void testMergeLength() {
+		FlexArray fa;
+        FlexArray ga;
+        FlexArray r;
+        
+        for (int i = 0; i < 10; i++) {
+            fa = new FlexArray(10, i);
+            ga = new FlexArray(10, 0);
+            r = fa.merge(ga);
+            assertEquals(10+i, r.length());
+            r = ga.merge(fa);
+            assertEquals(10+i, r.length());
+            
+            fa = new FlexArray(10, 0);
+            ga = new FlexArray(10-i, i);
+            r = fa.merge(ga);
+            assertEquals(10, r.length());
+            r = ga.merge(fa);
+            assertEquals(10, r.length());
+            
+            fa = new FlexArray(1+i, 0);
+            ga = new FlexArray(1+i, 0-i);
+            r = fa.merge(ga);
+            assertEquals(1+2*i, r.length());
+            r = ga.merge(fa);
+            assertEquals(1+2*i, r.length());
+            
+        }
+        
+        for (int i = 0; i < 5; i++) {
+            fa = new FlexArray(10, -5 + i);
+            ga = new FlexArray(10, 0);
+            r = fa.merge(ga);
+            assertEquals(15-i, r.length());
+            r = ga.merge(fa);
+            assertEquals(15-i, r.length());
+            
+            fa = new FlexArray(10, -7 + i);
+            ga = new FlexArray(20, -2);
+            r = fa.merge(ga);
+            assertEquals(25-i, r.length());
+            r = ga.merge(fa);
+            assertEquals(25-i, r.length());
+            
+            fa = new FlexArray(10, -17 + i);
+            ga = new FlexArray(20, -12);
+            r = fa.merge(ga);
+            assertEquals(25-i, r.length());
+            r = ga.merge(fa);
+            assertEquals(25-i, r.length());
+            
+            fa = new FlexArray(10, -37 + i);
+            ga = new FlexArray(20, -32);
+            r = fa.merge(ga);
+            assertEquals(25-i, r.length());
+            r = ga.merge(fa);
+            assertEquals(25-i, r.length());
+            
+        }
+        
+        fa = new FlexArray(20, -30);
+        ga = new FlexArray(5, -20);
+        r = fa.merge(ga);
+        assertEquals(20, r.length());
+        r = ga.merge(fa);
+        assertEquals(20, r.length());
+        
+        fa = new FlexArray(10, -2);
+        ga = new FlexArray(10, -8);
+        r = fa.merge(ga);
+        assertEquals(16, r.length());
+        r = ga.merge(fa);
+        assertEquals(16, r.length());
+        
+        fa = new FlexArray(1, -2);
+        ga = new FlexArray(1, -8);
+        r = fa.merge(ga);
+        assertEquals(null, r);
+        r = ga.merge(fa);
+        assertEquals(null, r);
+        
+        fa = new FlexArray(1, -2);
+        ga = new FlexArray(1, -2);
+        r = fa.merge(ga);
+        assertEquals(1, r.length());
+        r = ga.merge(fa);
+        assertEquals(1, r.length());
+        
+	}
+	
+	@Test
+	public void testMergeOther() {
+		FlexArray fa, ga, r;
+        int[] ra;
+
+        /* easier to understand tests
+         *
+         * 		1 1 1 1 1
+         * 			2 2 2 2 2
+         * 
+         * 			  |
+         * 			  V
+         * 		
+         * 		1 1 3 3 3 2 2
+         * 
+         */
+        
+        // ---
+        
+        int[] expected1 = new int[5];
+        fa = new FlexArray(5, 0);
+        ga = new FlexArray(5, 0);
+        for (int i=0; i<5; i++) {
+            fa.write(i, 1);
+            ga.write(i, 2);
+            expected1[i] = 3;
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected1, ra);
+        
+        // ---
+        
+        // basis shift by one
+        
+        int[] expected2 = {1, 3, 3, 3, 3, 2};
+        fa = new FlexArray(5, 0);
+        ga = new FlexArray(5, 1);
+        for (int i=0; i<5; i++) {
+            fa.write(i, 1);
+            ga.write(i+1, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected2, ra);
+        
+        // ---
+        
+        // basis shift into negative
+        
+        int[] expected3 = {2, 3, 3, 3, 3, 1};
+        fa = new FlexArray(5, 0);
+        ga = new FlexArray(5, -1);
+        for (int i=0; i<5; i++) {
+            fa.write(i, 1);
+            ga.write(i-1, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected3, ra);
+        
+        // ---
+        
+        // both bases shifted into negative
+        
+        int[] expected4 = {2, 2, 3, 3, 3, 1, 1};
+        fa = new FlexArray(5, -1);
+        ga = new FlexArray(5, -3);
+        for (int i=0; i<5; i++) {
+            fa.write(i-1, 1);
+            ga.write(i-3, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected4, ra);
+        
+        // ---
+        
+        // fa "contains" ga
+        
+        int[] expected5 = {1, 3, 3, 3, 1};
+        fa = new FlexArray(5, -2);
+        ga = new FlexArray(3, -1);
+        for (int i=0; i<5; i++) {
+            fa.write(i-2, 1);
+            ga.write(i-1, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected5, ra);
+        
+        // ---
+        
+        // ga "contains" fa
+        
+        int[] expected6 = {2, 3, 3, 3, 2};
+        fa = new FlexArray(3, -1);
+        ga = new FlexArray(5, -2);
+        for (int i=0; i<5; i++) {
+            fa.write(i-1, 1);
+            ga.write(i-2, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected6, ra);
+        
+        // ---
+        
+        // other
+        
+        int[] expected7 = {1, 3, 3, 2, 2, 2};
+        fa = new FlexArray(3, -2);
+        ga = new FlexArray(5, -1);
+        for (int i=0; i<5; i++) {
+            fa.write(i-2, 1);
+            ga.write(i-1, 2);
+        }
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected7, ra);
+        
+        int[] expected8 = {1, 3, 3, 7};
+        fa = new FlexArray(3, -2);
+        ga = new FlexArray(4, -2);
+        for (int i=0; i<3; i++) {
+            fa.write(i-2, 1);
+        }
+        
+        ga.write(-2, 0); ga.write(-1, 2); ga.write(0, 2); ga.write(1, 7);
+        
+        r = fa.merge(ga);
+        ra = r.rawArray();
+        
+        assertArrayEquals(expected8, ra);
+        
+	}
 }
